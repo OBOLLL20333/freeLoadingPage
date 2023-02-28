60acae99931d0e606a578cf973e0a72cc3c58d93
OBOLLL20333 <97986329+OBOLLL20333@users.noreply.github.com>
2023-03-01 00:57:48 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const button = document.querySelector('.burger-header__button')
+const buger = document.querySelector('.header ')
+button.onclick = function () {
+	buger.classList.toggle("_active");
+}
