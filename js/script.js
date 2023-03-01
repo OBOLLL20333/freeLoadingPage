c64efc3890fc078da74524313c99d47420fc63bf
OBOLLL20333 <97986329+OBOLLL20333@users.noreply.github.com>
2023-03-01 21:13:33 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
-const button = document.querySelector('.burger-header__button')
-const buger = document.querySelector('body')
+const button = document.querySelector('.burger-header__button');
+const buger = document.querySelector('body');
+
 button.onclick = function () {
 	buger.classList.toggle("_active");
+}
+
+const menuLinks = document.querySelectorAll('.header__link[data-goto]');
+
+if (menuLinks.length > 0) {
+	menuLinks.forEach(menuLink => {
+		menuLink.addEventListener('click', onMenuLinkClick);
+	});
+
+	function onMenuLinkClick(e) {
+		const menuLink = e.target;
+		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
+
+			window.scrollTo({
+				top: gotoBlockValue,
+				behavior: 'smooth'
+			});
+			e.preventDefault();
+			buger.classList.remove('_active')
+		}
+	}
 }
